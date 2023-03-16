7f2dde9c6a7baf644cae54e3b6848670a51fbfbf
Anton Kablukov <antoha7@gmail.com>
2023-03-16 20:03:05 +0100

    test: should return Fizz if multiple of 3

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from './fizzbuzz';
 
-describe("fizzbuzz", () => {
-
-
+describe('fizzbuzz', () => {
+  it('should return Fizz, if the provided argument is multiple of 3', () => {
+    expect(fizzbuzz(3)).toBe('Fizz');
+  });
 });
