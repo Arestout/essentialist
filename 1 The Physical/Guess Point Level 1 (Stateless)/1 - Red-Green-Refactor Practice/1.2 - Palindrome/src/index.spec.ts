b67dc926cbfe331cf829b77c2f960b53309e4070
Anton Kablukov <antoha7@gmail.com>
2023-03-18 13:19:30 +0100

    test: return false if phrase is not palindrome and has spaces and different casing

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -16,4 +16,8 @@ describe('palindrome checker', () => {
   it('should return true if the provided phrase is palindrome and has spaces and different casing', () => {
     expect(palindromeChecker('Was It A Rat I Saw')).toBe(true);
   });
+
+  it('should return false if the provided phrase is not palindrome and has spaces and different casing', () => {
+    expect(palindromeChecker('WaT Ist A Raft I Saaw')).toBe(false);
+  });
 });
