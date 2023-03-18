0026da39b708708a99ccc452acff8c23fd69bb22
Anton Kablukov <antoha7@gmail.com>
2023-03-18 13:34:21 +0100

    refactor: replace toBe with toBeTruthy or toBeFalsy

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -2,22 +2,22 @@ import { palindromeChecker } from './index';
 
 describe('palindrome checker', () => {
   it('should return true if the provided phrase is palindrome', () => {
-    expect(palindromeChecker('mom')).toBe(true);
+    expect(palindromeChecker('mom')).toBeTruthy();
   });
 
   it('should return false if the provided phrase is not palindrome', () => {
-    expect(palindromeChecker('bill')).toBe(false);
+    expect(palindromeChecker('bill')).toBeFalsy();
   });
 
   it('should return true if the provided phrase is palindrome and is in capital case', () => {
-    expect(palindromeChecker('Mom')).toBe(true);
+    expect(palindromeChecker('Mom')).toBeTruthy();
   });
 
   it('should return true if the provided phrase is palindrome and has spaces and different casing', () => {
-    expect(palindromeChecker('Was It A Rat I Saw')).toBe(true);
+    expect(palindromeChecker('Was It A Rat I Saw')).toBeTruthy();
   });
 
   it('should return false if the provided phrase is not palindrome and has spaces and different casing', () => {
-    expect(palindromeChecker('WaT Ist A Raft I Saaw')).toBe(false);
+    expect(palindromeChecker('WaT Ist A Raft I Saaw')).toBeFalsy();
   });
 });
