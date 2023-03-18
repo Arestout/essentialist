c005449ec9ccd6fcebf69fa4ac7bdfa36d8206aa
Anton Kablukov <antoha7@gmail.com>
2023-03-18 11:55:40 +0100

    test: return true if the provided word is a palindrome

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -1,4 +1,6 @@
-
+import { palindromeChecker } from './index';
 describe('palindrome checker', () => {
-
-})
+  it('should return true if the provided word is palindrome', () => {
+    expect(palindromeChecker('mom')).toBe(true);
+  });
+});
---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -0,0 +1,3 @@
+export function palindromeChecker(word: string) {
+  return true;
+}
