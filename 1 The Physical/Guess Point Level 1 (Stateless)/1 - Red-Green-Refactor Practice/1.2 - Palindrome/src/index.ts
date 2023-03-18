162693d29f246f2acd5abdee7ffda7e84310dc63
Anton Kablukov <antoha7@gmail.com>
2023-03-18 13:24:30 +0100

    refactor: extract string reversal to a helper function

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -1,5 +1,7 @@
+const getReversedString = (str: string) => str.split('').reverse().join('');
+
 export function palindromeChecker(phrase: string) {
-  const reversedPhrase = phrase.split('').reverse().join('');
+  const reversedPhrase = getReversedString(phrase);
 
   return phrase.toLocaleLowerCase().split(' ').join('') === reversedPhrase.toLocaleLowerCase().split(' ').join('');
 }
