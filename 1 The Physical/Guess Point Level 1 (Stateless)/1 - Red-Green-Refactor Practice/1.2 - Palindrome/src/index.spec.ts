a75147660a823063b68d050d1df2755836105650
Anton Kablukov <antoha7@gmail.com>
2023-03-18 13:15:59 +0100

    test: return true if a phrase is palindrome and has spaces and different casing

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.spec.ts
@@ -12,4 +12,8 @@ describe('palindrome checker', () => {
   it('should return true if the provided phrase is palindrome and is in capital case', () => {
     expect(palindromeChecker('Mom')).toBe(true);
   });
+
+  it('should return true if the provided phrase is palindrome and has spaces and different casing', () => {
+    expect(palindromeChecker('Was It A Rat I Saw')).toBe(true);
+  });
 });
--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -1,5 +1,5 @@
 export function palindromeChecker(phrase: string) {
   const reversedPhrase = phrase.split('').reverse().join('');
 
-  return phrase.toLocaleLowerCase() === reversedPhrase.toLocaleLowerCase();
+  return phrase.toLocaleLowerCase().split(' ').join('') === reversedPhrase.toLocaleLowerCase().split(' ').join('');
 }
