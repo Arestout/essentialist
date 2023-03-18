59bd996ed2b89a89c81d86344b7247cba9fc6b49
Anton Kablukov <antoha7@gmail.com>
2023-03-18 13:32:21 +0100

    refactor: extract toLowerCase and space removal to a helper function

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -1,7 +1,8 @@
 const getReversedString = (str: string) => str.split('').reverse().join('');
+const normalizeString = (str: string) => str.toLocaleLowerCase().split(' ').join('');
 
 export function palindromeChecker(phrase: string) {
   const reversedPhrase = getReversedString(phrase);
 
-  return phrase.toLocaleLowerCase().split(' ').join('') === reversedPhrase.toLocaleLowerCase().split(' ').join('');
+  return normalizeString(phrase) === normalizeString(reversedPhrase);
 }
