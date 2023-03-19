1b009dab336f9578c641b610282970a68d3ca8b4
Anton Kablukov <antoha7@gmail.com>
2023-03-19 16:59:10 +0100

    [new]: return validation result without errors

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,10 @@
+import { validatePassword } from './index';
 
 describe('password validator', () => {
+  it('returns validation result without errors', () => {
+    const validationResponse = validatePassword('Testpass78%');
 
-})
-
+    expect(validationResponse.result).toBeTruthy();
+    expect(validationResponse.errors.length).toBe(0);
+  });
+});
