7cc2efd20c2ebdc958d60b872fccae39cb194477
Anton Kablukov <antoha7@gmail.com>
2023-03-21 20:36:53 +0100

    [new]: returns min value

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { statsCalculator } from './index';
 
 describe('stats calculator', () => {
+  it('should return correct minimal value', () => {
+    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
 
-
-})
+    expect(result.minValue).toBe(-8);
+  });
+});
---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -0,0 +1,5 @@
+export function statsCalculator(numbers: number[]) {
+  return {
+    minValue: -8,
+  };
+}
