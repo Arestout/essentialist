1fe1a166fdd9af54508a12eda3534511d6675d98
Anton Kablukov <antoha7@gmail.com>
2023-03-21 20:52:57 +0100

    [refactor]: extract calculator input to a const

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -1,20 +1,22 @@
 import { statsCalculator } from './index';
 
+const CALCULATOR_INPUT = [2, 4, 21, -8, 53, 40];
+
 describe('stats calculator', () => {
   it('should return correct minimal value', () => {
-    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    const result = statsCalculator(CALCULATOR_INPUT);
 
     expect(result.minValue).toBe(-8);
   });
 
   it('should return correct max value', () => {
-    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    const result = statsCalculator(CALCULATOR_INPUT);
 
     expect(result.maxValue).toBe(53);
   });
 
   it('should return correct elements count', () => {
-    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    const result = statsCalculator(CALCULATOR_INPUT);
 
     expect(result.elementsCount).toBe(6);
   });
